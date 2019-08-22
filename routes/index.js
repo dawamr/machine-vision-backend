8659ba451f9a31e5bae4a845f7336828802322d5
yuanvirna <yuvirna93@gmail.com>
2019-08-22 15:04:50 +0700

    adding crud method routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
+const parameters_controller = require('../controllers/index').parameters_index;
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the MV API!',
   }));
+
+  app.get('/api/parameters', parameters_controller.list);
+  app.post('/api/parameters', parameters_controller.create);
+  app.put('/api/parameters', parameters_controller.update);
+  app.delete('/api/parameters', parameters_controller.destroy);
+
 }
