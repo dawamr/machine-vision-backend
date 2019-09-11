6ef3c28c522bc392c7555c85916dda9ef48079b3
nesya_r <nesyarogawati@gmail.com>
2019-09-11 13:24:43 +0700

    [nesya]edit product response

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,22 +1,34 @@
 const product = require('../models').product;
+const product2 = require('../models').product;
 const productCategory = require('../models').product_category;
 const resp = require('../views/response');
 const pagination = require('../utils/pagination');
 const sequelize = require('sequelize');
 
 module.exports = {
-  create(req, res){
+  create(req, res, next) {
     return product
       .create({
         name: req.body.name,
-        product_category_id: req.body.product_category_id,
+        product_category_id: req.body.product_category_id
       })
-      .then(product => {
-        resp.ok(true, "Success create product.", product.dataValues, res);
+      .then(result => {
+        console.log(result.dataValues.id)
+        return product.findOne({
+            where: {
+              id: result.dataValues.id
+            },
+            include: [{
+              model: productCategory,
+            }]
+          })
+          .then(newData => {
+            req.data = newData;
+            resp.ok(true, "Success add data product.", req.data, res)
+          })
       })
       .catch((error) => {
-        resp.ok(false, "Failed create product.", null, res.status(400));
-        console.log(error);
+        resp.ok(false, "Failed add data product.", null, res.status(400));
       });
   },
 
@@ -39,14 +51,18 @@ module.exports = {
     if ((req.query.per_page != undefined) && (req.query.per_page.length > 0)) {
       perPage = req.query.per_page;
     }
-    if ((req.query.search != undefined) && (req.query.search.length > 0)){
+    if ((req.query.search != undefined) && (req.query.search.length > 0)) {
       options.name = sequelize.where(sequelize.fn('LOWER', sequelize.col('product.name')), 'LIKE', '%' + req.query.search + '%');
     }
-    if ((req.query.product_category_id != undefined) && (req.query.product_category_id.length > 0)){
+    if ((req.query.product_category_id != undefined) && (req.query.product_category_id.length > 0)) {
       options.product_category_id = sequelize.where(sequelize.col('product.product_category_id'), '=', req.query.product_category_id);
     }
 
-    let { offsetResult, perPageResult, showPageResult } = pagination.builder(perPage, page);
+    let {
+      offsetResult,
+      perPageResult,
+      showPageResult
+    } = pagination.builder(perPage, page);
 
     return product
       .findAndCountAll({
@@ -57,7 +73,7 @@ module.exports = {
         order: [
           [orderBy, sortBy]
         ],
-        limit:  perPageResult,
+        limit: perPageResult,
         offset: offsetResult,
       })
       .then(productResult => {
@@ -73,20 +89,34 @@ module.exports = {
   },
 
   listAll(req, res, next) {
+    let orderBy = 'created_at';
+    let sortBy = 'desc';
+    let options = {};
 
-    return product
-    .findAll({
-      include: [{
-        model: productCategory
-      }]
-    })
-    .then(productResult => {
-          resp.ok(true, "Get all data product.", productResult, res);
-        })
-        .catch((error) => {
-          resp.ok(false, "Failed get all data product.", null, res.status(400));
-          console.log(error);
-        });
+    if ((req.query.order_by != undefined) && (req.query.order_by.length > 0)) {
+      orderBy = req.query.order_by;
+    }
+    if ((req.query.sort_by != undefined) && (req.query.sort_by.length > 0)) {
+      sortBy = req.query.sort_by;
+    }
+    if ((req.query.search != undefined) && (req.query.search.length > 0)){
+      options.name = sequelize.where(sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', '%' + req.query.search + '%');
+    }
+
+    return processMachine
+      .findAll({
+        where: options,
+        order: [
+          [orderBy, sortBy]
+        ],
+      })
+      .then(processMachineResult => {
+        resp.ok(true, "Get all data process machine.", processMachineResult, res);
+      })
+      .catch((error) => {
+        resp.ok(false, "Failed get all data process machine.", null, res.status(400));
+        console.log(error);
+      });
   },
 
   detail(req, res) {
@@ -110,7 +140,11 @@ module.exports = {
 
   update(req, res) {
     return product
-      .findByPk(req.params.id)
+      .findOne({
+        where: {
+          id: req.params.id
+        }
+      })
       .then(product => {
         if (!product) {
           resp.ok(false, "product not found.", null, res.status(400));
@@ -122,7 +156,15 @@ module.exports = {
             product_category_id: req.body.product_category_id || product.product_category_id,
           })
           .then(product => {
-            resp.ok(true, "Success update product.", product.dataValues, res);
+            return product2
+              .findByPk(product.id, {
+                include: [{
+                  model: productCategory
+                }]
+              })
+              .then(result => {
+                resp.ok(true, "Success delete product.", result.dataValues, res);
+              })
           })
           .catch((error) => {
             resp.ok(false, "Failed update product.", null, res.status(400));
