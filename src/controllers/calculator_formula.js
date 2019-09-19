2e6da97c5b6ea5a8cb2ab44d557e40d9f1f4bfb5
Dawam Raja <akunnyadawam9@gmail.com>
2019-09-19 12:07:29 +0700

    add calculator_formula controller

---
+++ src/controllers/calculator_formula.js
@@ -0,0 +1,9 @@
+// console.log(Object.keys(require('../models')));
+const CalculatorFormula = require('../models').calculator_formula;
+
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
+
+module.exports = {
+
+}
