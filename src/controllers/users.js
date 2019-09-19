f15bbcb7b90af719ace4fbef8f1680f546fd30a7
sandri <sandriwinata@gmail.com>
2019-09-19 15:46:13 +0700

    fix login

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -19,9 +19,9 @@ const sequelize = require('sequelize');
 
 function UserController() {
 
-  const login = (req, res, next) => {
-    let username = 'implementor';
-    let password = '123456';
+  const login = (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
 
     AuthenticationService.authenticate(username, password)
       .then((user) => {
