182842d0aecdd6e4bfdc8adc8292aeac48c7c4d7
Dawam Raja <akunnyadawam9@gmail.com>
2019-09-20 06:57:06 +0700

    delete name on model and migration

--- src/migrations/20190919043035-create-calculator-formula.js
+++ src/migrations/20190919043035-create-calculator-formula.js
@@ -8,9 +8,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      name: {
-        type: Sequelize.STRING
-      },
       level: {
         type: Sequelize.ENUM('plant', 'sector', 'line', 'machine')
       },
--- src/models/calculator_formula.js
+++ src/models/calculator_formula.js
@@ -1,7 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const calculator_formula = sequelize.define('calculator_formula', {
-    name: DataTypes.STRING,
     level: DataTypes.ENUM('plant', 'sector', 'line', 'machine'),
     level_reference_id: DataTypes.ENUM('plant', 'sector', 'line', 'machine'),
     formula_script: DataTypes.TEXT,
