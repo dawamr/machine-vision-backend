5ff9bf50c239a42f8f305917cc0553b2580dd172
Dawam Raja <akunnyadawam9@gmail.com>
2019-09-23 07:50:31 +0700

    list sensor and line - calculator list

--- src/controllers/calculator_formula.js
+++ src/controllers/calculator_formula.js
@@ -1,5 +1,10 @@
 // console.log(Object.keys(require('../models')));
-const CalculatorFormula = require('../models').calculator_formula;
+const formula = require('../models').calculator_formula;
+const sector = require('../models').sector;
+const line = require('../models').line;
+const proses_machine = require('../models').process_machine;
+const proses = require('../models').process;
+const machine = require('../models').machine;
 
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op
@@ -7,7 +12,19 @@ const Op = Sequelize.Op
 module.exports = {
 
     listAll(req, res){
-        res.json('testing')
+        // let sensors = sensor.findAll({})
+        // let lines = line.findAll({})
+        line.findAll({
+            attributes:['name'],
+            include: [{
+                model: sector,
+              }]
+        })
+        .then(result => res.status(201).send(result))
+        .catch(error => res.status(400).send(error));
+        // Promise.all([sensors,lines])
+        // .then(result => res.status(201).send(result))
+        // .catch(error => res.status(400).send(error));
     }
 
 }
