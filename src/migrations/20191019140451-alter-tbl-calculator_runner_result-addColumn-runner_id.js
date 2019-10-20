ec7db5694407a0e334296679508958f669c253ec
Dawam Raja <akunnyadawam9@gmail.com>
2019-10-20 08:26:57 +0700

    fix-migration

--- src/migrations/20191019140451-alter-tbl-calculator_runner_result-addColumn-runner_id.js
+++ src/migrations/20191019140451-alter-tbl-calculator_runner_result-addColumn-runner_id.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('calculator_runner_result', 'runner_id', {
+    return queryInterface.addColumn('calculator_runner_results', 'runner_id', {
       type: Sequelize.INTEGER,
     });
   },
 
   down: (queryInterface, Sequelize) => {
-    queryInterface.removeColumn('calculator_runner_result', 'runner_id');
+    queryInterface.removeColumn('calculator_runner_results', 'runner_id');
   }
 };
