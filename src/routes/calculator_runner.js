e2b454cbfea012dfb33fdc151eabc1fe32a39248
Dawam Raja <akunnyadawam9@gmail.com>
2019-11-07 11:42:58 +0700

    updated calculator

--- src/routes/calculator_runner.js
+++ src/routes/calculator_runner.js
@@ -4,9 +4,13 @@ const express = require('express')
 const runner = express.Router();
 
 
+runner.get('/runner/result/list',calRunner.resultAll)
+runner.get('/runner/result/:id',calRunner.resultDetail)
+runner.get('/runner/:id/list', calRunner.resultList)
 runner.get('/runner/list', calRunner.runnerList);
 runner.get('/runner/:id', calRunner.runnerDetail);
 runner.post('/runner', calRunner.runnerRun);
+
 
 runner.post('/runner/:id/result', calRunner.storeResult)
 
