a40b40bec8d6b88e516d292b4b3dcd67d13c4967
Dawam Raja <akunnyadawam9@gmail.com>
2019-11-26 17:07:20 +0700

    update db

--- src/migrations/20191018152437-alter-tbl-parameter-builder-change-type.js
+++ src/migrations/20191018152437-alter-tbl-parameter-builder-change-type.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    // queryInterface.removeColumn('parameters', 'type');
-    return queryInterface.addColumn('parameters', 'type', {
-      type: Sequelize.STRING,
-    });
+    // // queryInterface.removeColumn('parameters', 'type');
+    // return queryInterface.addColumn('parameters', 'type', {
+    //   type: Sequelize.STRING,
+    // });
   },
 
   down: (queryInterface, Sequelize) => {
-    queryInterface.removeColumn('parameters', 'type');
+    // queryInterface.removeColumn('parameters', 'type');
   }
 };
